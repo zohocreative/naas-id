f3ae6f3e24714733cc92d4fa9e9d870159f67489
zohocreative <zohoagen@gmail.com>
2026-09-21 15:11:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,4 @@
  * Every time naas_engine.gs changes, use "Manage deployments ▸ Edit ▸
  * New version" (not a brand-new deployment) so this URL keeps working.
  * ==================================================================== */
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQOz_sZVE7SLrVOd_gRo0_30ohF7RUxL2I-l6l3yp3-maQRRISmmd_l5zFQj602oV9BI17RMS390B9zskFEWZ6U4RhuBtyMPcvGPKIpob3Tytd91kptJsfi2eLwrsnVkpnFP-l0rhjXOLNB02ZAFHFfTrbc6lKnT3zVgSa1azaQtZushacinpiQCmWPnuaM3iZc7frbAz_ctJFZDaKq1YfFVTVK2fpR03FR5Nxrlhe2fGCDLZ2veIrgDuT0aio54dXMaE5fYDVjMtJjmzlFj-cDR04IMQ&lib=MO9kDQaEZ2sZSPgydZRppHdd27P5RNkdl";
